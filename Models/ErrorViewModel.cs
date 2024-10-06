090b16d6b70e5e619d5ddc8436d33fa2a08a3fb7
Socratis <socratis.gomes.silva@gmail.com>
2024-10-06 14:37:36 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_gestao_de_proventos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
